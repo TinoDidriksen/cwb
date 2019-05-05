TinoDidriksen/cwb

commit da68fd516b77685b6327ef24ad5a25f6fdfdd322

Add new show option (show curent/active) to print the activated corpus (useful in slacve mode)
git-svn-id: https://svn.code.sf.net/p/cwb/code/cwb/trunk@1075 b72dbb9a-5b02-45d8-a000-b69eecba7de2

cqp/parser.y
@@ -79,23 +79,27 @@ void warn_query_lock_violation(void) {
 
 /* ============================================================ */
 
-/* note: SYNCHRONIZE is a windows API identifier, and it doesn't seem at all
-necessary here - it is just defined, then tested.
-So: commented out. AH 2/4/2010
-#define SYNCHRONIZE
-*/
 
+/**
+ * Deletes any still-pending input to the CQP parser, to the next
+ * semi-colon or end of line.
+ */ 
 void
 synchronize(void)
 {
-/*#if defined(SYNCHRONIZE)*/
-  int macro_status;
+/* function may be left uncompiled by defining this constant
+ * in the make-configuration file. See the Makefile.         */
+#ifdef CQP_COMPILE_WITHOUT_SYNCHRONIZE
+
+  int macro_status; /*  stores enable_macros status */
 
   /* delete macro buffers & disable macro expansion while sync'ing */
   delete_macro_buffers(1); /* print stack trace on STDERR */
   macro_status = enable_macros;
   enable_macros = 0;
 
+  /* read and throw away characters till we have cleared away 
+   * the rest of what's pending. */
   if (cqp_input_string != NULL) {
     fprintf(stderr, "Synchronizing to end of line ... \n");
     while (!(yychar <= 0))
@@ -107,8 +111,10 @@ synchronize(void)
       yychar = yylex();
   }
 
-  enable_macros = macro_status; /* reset enable_macros to previous value */
-/*#endif*/
+  /* restore enable_macros to previous value */
+  enable_macros = macro_status; 
+
+#endif /* defined CQP_COMPILE_WITHOUT_SYNCHRONIZE */
 }
 
 #define YYERROR_VERBOSE
@@ -499,11 +505,14 @@ Showing:          SHOW_SYM              {
                                           if (strncasecmp($2, "var", 3) == 0) {
                                             do_PrintAllVariables();
                                           }
-                                          else if ((strncasecmp($2, "sys", 3) == 0) || (strncasecmp($2, "corp", 4) == 0)) {
+                                          else if (strncasecmp($2, "active", 6) == 0 || strncasecmp($2, "current", 7) == 0) {
+                                            show_corpus_active();
+                                          }
+                                          else if (strncasecmp($2, "sys", 3) == 0 || strncasecmp($2, "corp", 4) == 0) {
                                             show_corpora_files(SYSTEM);
                                           }
-                                          else if ((strncasecmp($2, "sub", 3) == 0) || (strcasecmp($2, "named") == 0) || (strcasecmp($2, "queries") == 0)) {
-                                            show_corpora_files(SUB);    
+                                          else if (strncasecmp($2, "sub", 3) == 0 || strcasecmp($2, "named") == 0 || strcasecmp($2, "queries") == 0) {
+                                            show_corpora_files(SUB);
                                           }
                                           else {
                                             cqpmessage(Error, "show what?");
@@ -511,7 +520,7 @@ Showing:          SHOW_SYM              {
                                         }
                 | SHOW_SYM 
                   AttributeSelections   /* the actual work is done there */
-                | SHOW_SYM
+                | SHOW_SYM 
                   CD_SYM                { PrintContextDescriptor(&CD); }
                 ;
 
@@ -1240,16 +1249,16 @@ ReStructure:    EXPAND_SYM OptDirection
                                           expansion.size = $4.size;
                                           expansion.attrib = $4.attrib;
                                         }
-              | /* epsilon */           { expansion.direction = leftright;
+              | /* epsilon */           { expansion.direction = ctxtdir_leftright;
                                           expansion.type = word;
                                           expansion.size = 0;
                                           expansion.attrib = NULL;
                                         }
                 ;
 
-OptDirection:   LEFT_SYM               { $$ = left; }
-              | RIGHT_SYM              { $$ = right; }
-              | /* epsilon */          { $$ = leftright; }
+OptDirection:   LEFT_SYM               { $$ = ctxtdir_left; }
+              | RIGHT_SYM              { $$ = ctxtdir_right; }
+              | /* epsilon */          { $$ = ctxtdir_leftright; }
                 ;
 
 Description:    OptNumber ID           { do_Description(&($$), $1, $2); }
@@ -1544,8 +1553,8 @@ ShowMacro:        SHOW_SYM MACRO_SYM    {
                                         }
                 ;
 
-/* a list of strings is concatenated into a single string, in order */
-/* to allow multi-line macro definitions with comments */
+/* a list of strings is concatenated into a single string, in order *
+ * to allow multi-line macro definitions with comments */
 MultiString:      MultiString STRING    {
                                           int l1 = strlen($1), l2 = strlen($2);
                                           char *s = (char *) cl_malloc(l1 + l2 + 2);

definitions.mk
@@ -23,7 +23,7 @@
 #
 # CWB version
 #
-VERSION = 3.4.14
+VERSION = 3.4.15
 # 3.4.x = beta versions leading up to new stable 3.5.0
 
 #

cl/macros.c
@@ -1,13 +1,13 @@
-/* 
+/*
  *  IMS Open Corpus Workbench (CWB)
  *  Copyright (C) 1993-2006 by IMS, University of Stuttgart
  *  Copyright (C) 2007-     by the respective contributers (see file AUTHORS)
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the
  *  Free Software Foundation; either version 2, or (at your option) any later
  *  version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
@@ -22,13 +22,25 @@
 #include <time.h>
 
 
+/**
+ * @file
+ *
+ * This file contains a number of generally handy functyions:
+ *
+ * - memory  management (better malloc, free, etc.)
+ * - printing of indented lists
+ * - progress bar printing.
+ *
+ * NB. The latter two things probably ought to be in CQP rather than here.
+ */
+
 
 
 
 
 /*
- * memory allocation functions with integrate success test 
- * (this functions will be used as hooks for the CL MMU)
+ * memory allocation functions with integrated success test
+ * (to be used as hooks for the CL MMU)
  */
 
 /**
@@ -94,7 +106,7 @@ cl_realloc(void *block, size_t bytes)
 {
   void *new_block;
 
-  if (block == NULL) 
+  if (block == NULL)
     new_block = malloc(bytes);	/* some OSs don't fall back to malloc() if block == NULL */
   else
     new_block = realloc(block, bytes);
@@ -103,7 +115,7 @@ cl_realloc(void *block, size_t bytes)
     if (bytes == 0) {
       /* don't warn any more, reallocating to 0 bytes should create no problems, at least on Linux and Solaris */
       /* (the message was probably shown on Linux only, because Solaris doesn't return NULL in this case) */
-      /* fprintf(stderr, "CL: WARNING realloc() to 0 bytes!\n"); */      
+      /* fprintf(stderr, "CL: WARNING realloc() to 0 bytes!\n"); */
     }
     else {
       fprintf(stderr, "CL: Out of memory. (killed)\n");
@@ -170,10 +182,10 @@ cl_set_rng_state(unsigned int i1, unsigned int i2)
  * @param i1  Target location for the value of RNG_I1
  * @param i2  Target location for the value of RNG_I2
  */
-void 
+void
 cl_get_rng_state(unsigned int *i1, unsigned int *i2)
 {
-  *i1 = RNG_I1; 
+  *i1 = RNG_I1;
   *i2 = RNG_I2;
 }
 
@@ -190,6 +202,7 @@ cl_set_seed(unsigned int seed)
 
 /**
  *  Initialises the CL-internal random number generator from the current system time.
+ *  TODO, maybe this name would be bbetter as "cl_seed_time"?
  */
 void
 cl_randomize(void)
@@ -218,8 +231,10 @@ cl_random(void)
  * Part of the CL-internal random number generator.
  *
  * @return  The generated random number.
+ * TODO runif sounds a bit too much like "run if". I (AH) keep getting xconfused.
+ *       .... maybe cl_random_fraction?
  */
-double 
+double
 cl_runif(void)
 {
   return cl_random() * 2.328306437080797e-10; /* = cl_random / (2^32 - 1) */
@@ -233,6 +248,7 @@ cl_runif(void)
 
 /*
  *  display progress bar in terminal window
+ *  TODO: put in another file maybe??
  */
 
 /* non-exported global variables for progress bar */
@@ -328,7 +344,9 @@ progress_bar_percentage(int pass, int total, int percentage)
 
 
 /*
- *  print indented 'tabularised' lists
+ *  print indented 'tabularised' lists.
+ *
+ *  Note that use of global variables makes this non-reentrant
  */
 
 /* status variables (non-exported globals) */
@@ -368,7 +386,7 @@ start_indented_list(int linewidth, int tabsize, int indent)
   ilist_cursor = 0;
   /* indent from left margin */
   ilist_print_blanks(ilist_indent);
-} 
+}
 
 /**
  * Starts a new line in an indented 'tabularised' list.
@@ -383,7 +401,7 @@ void
 print_indented_list_br(char *label)
 {
   int llen = (label != NULL) ? strlen(label) : 0;
-  
+
   if (ilist_cursor != 0) {
     printf("\n");
   }

cl/macros.h
@@ -53,7 +53,7 @@
  * they are considered non-equal when one of both is NULL.
  * @param a  the first string
  * @param b  the second string
- * @return   a Boolean
+ * @return   Boolean
  */
 #define STREQ(a,b) (((a) == (b)) || \
                     ((a) && (b) && (strcmp((a), (b)) == 0)))

cqp/Makefile
@@ -18,12 +18,6 @@
 
 ##  This is the Makefile for the CWB Corpus Query Processor programs. 
 
-
-
-## Read configuration settings and standard definitions
-TOP = $(shell pwd)/..
-include $(TOP)/config.mk
-
 ## The following targets are available:
 #
 #  all          create binaries cqp, cqpcl, and cqpserver
@@ -35,6 +29,18 @@ include $(TOP)/config.mk
 #  release      install to binary release dir
 #  size         print size of source code (line counts)
 #
+
+
+## Read configuration settings and standard definitions
+TOP = $(shell pwd)/..
+include $(TOP)/config.mk
+
+## This is an extra flag for this folder. It is used in the CQP parser; 
+## normally it is not defined, but if it is, the synchronize#() function 
+## becomes a no-op.
+ifdef CQP_COMPILE_WITHOUT_SYNCHRONIZE
+CFLAGS_ALL := -DCQP_COMPILE_WITHOUT_SYNCHRONIZE $(CFLAGS_ALL) 
+endif
 
 .PHONY: all clean realclean depend install uninstall size
 

cqp/ascii-print.c
@@ -1,13 +1,13 @@
-/* 
+/*
  *  IMS Open Corpus Workbench (CWB)
  *  Copyright (C) 1993-2006 by IMS, University of Stuttgart
  *  Copyright (C) 2007-     by the respective contributers (see file AUTHORS)
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the
  *  Free Software Foundation; either version 2, or (at your option) any later
  *  version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
@@ -55,7 +55,7 @@
 /* ---------------------------------------------------------------------- */
 
 /** [TODO get rid?] doesn't currently appear to be used anywhere? */
-#define USE_OLD_COMPOSE  
+#define USE_OLD_COMPOSE
 
 /* ---------------------------------------------------------------------- */
 
@@ -83,7 +83,7 @@ char *ascii_print_field(FieldType field, int at_end);
  */
 PrintDescriptionRecord ASCIIPrintDescriptionRecord = {
   "%9d: ",                            /* CPOSPrintFormat */
-  
+
   NULL,                               /* BeforePrintStructures */
   " ",                                /* PrintStructureSeparator */
   ": ",                               /* AfterPrintStructures */
@@ -117,7 +117,7 @@ PrintDescriptionRecord ASCIIPrintDescriptionRecord = {
  */
 PrintDescriptionRecord ASCIIHighlightedPrintDescriptionRecord = {
   "%9d: ",                            /* CPOSPrintFormat */
-  
+
   NULL,                               /* BeforePrintStructures */
   " ",                                /* PrintStructureSeparator */
   ": ",                               /* AfterPrintStructures */
@@ -231,7 +231,7 @@ get_screen_escapes(void)
 
   if ((setupterm(term, 1, &status) == ERR) || (status != 1)) {
     return;
-  }  
+  }
 
   /* turn off all attributes */
   sc_all_out = tigetstr("sgr0");
@@ -258,7 +258,7 @@ get_screen_escapes(void)
   if (sc_u_in == NULL) sc_u_in = sc_s_in;
   sc_u_out = tigetstr("rmul");
   if (sc_u_out == NULL) sc_u_out = sc_s_out;
-  
+
   /* bold */
   sc_b_in = tigetstr("bold");
   if (sc_b_in == NULL) {
@@ -281,7 +281,7 @@ get_screen_escapes(void)
   }
 
   escapes_initialized++;
-  
+
   /* in highlighted mode, switch off display attributes at end of line (to be on the safe side) */
   ASCIIHighlightedPrintDescriptionRecord.AfterLine = cl_malloc(strlen(sc_all_out) + 2);
   sprintf(ASCIIHighlightedPrintDescriptionRecord.AfterLine,
@@ -331,7 +331,7 @@ get_typeface_escape(char typeface)
 char *
 get_colour_escape(char colour, int foreground) {
   if (use_colour) {
-    if (*(get_typeface_escape('n')) == 0) 
+    if (*(get_typeface_escape('n')) == 0)
       return "";                /* don't try colour if terminal doesn't support typefaces */
     if (foreground) {
       switch(colour) {
@@ -417,7 +417,7 @@ ascii_print_field(FieldType field, int at_end)
     else
       sc_s_mode = 1;
     break;
-    
+
   case KeywordField:
     if (at_end)
       sc_u_mode = 0;
@@ -436,7 +436,7 @@ ascii_print_field(FieldType field, int at_end)
   default:
     break;
   }
-  
+
   /* now compose escape sequence which has to be sent to the terminal (setting _all_ attributes to their current values) */
   sprintf(sc_before_token + strlen(sc_before_token),
           "%s%s%s%s",
@@ -459,7 +459,7 @@ ascii_print_field(FieldType field, int at_end)
  * @param line            Character data of the line of aligned-corpus data to print. This is treated as opaque.
  */
 void
-ascii_print_aligned_line(FILE *stream, 
+ascii_print_aligned_line(FILE *stream,
                          int highlighting,
                          char *attribute_name,
                          char *line)
@@ -468,7 +468,7 @@ ascii_print_aligned_line(FILE *stream,
     char *red = get_colour_escape('r', 1);
     char *bold = get_typeface_escape('b');
     char *normal = get_typeface_escape('n');
-    fprintf(stream, "%s%s-->%s:%s %s\n", 
+    fprintf(stream, "%s%s-->%s:%s %s\n",
             red, bold,
             attribute_name,
             normal,
@@ -486,7 +486,7 @@ ascii_print_aligned_line(FILE *stream,
  *
  *
  */
-void 
+void
 print_concordance_line(FILE *outfd,
                        CorpusList *cl,
                        int element,
@@ -502,7 +502,7 @@ print_concordance_line(FILE *outfd,
     cqpmessage(Error, "Empty corpus or empty output file");
     return;
   }
-  
+
   if (element < 0 || element >= cl->size) {
     cqpmessage(Error, "Illegal element in print_concordance_line");
     return;
@@ -520,11 +520,11 @@ print_concordance_line(FILE *outfd,
   clf[MatchField].type = MatchField;
   clf[MatchField].start_position = cl->range[element].start;
   clf[MatchField].end_position = cl->range[element].end;
-      
+
   clf[MatchEndField].type = MatchEndField; /* unused, because we use MatchField for the entire match */
   clf[MatchEndField].start_position = -1;
   clf[MatchEndField].end_position = -1;
-      
+
   clf[KeywordField].type = KeywordField;
   if (cl->keywords) {
     clf[KeywordField].start_position = cl->keywords[element];
@@ -534,7 +534,7 @@ print_concordance_line(FILE *outfd,
     clf[KeywordField].start_position = -1;
     clf[KeywordField].end_position = -1;
   }
-      
+
   clf[TargetField].type = TargetField;
   if (cl->targets) {
     clf[TargetField].start_position = cl->targets[element];
@@ -550,7 +550,7 @@ print_concordance_line(FILE *outfd,
   else
     pdr = &ASCIIPrintDescriptionRecord;
 
-  outstr = compose_kwic_line(cl->corpus, 
+  outstr = compose_kwic_line(cl->corpus,
                              cl->range[element].start, cl->range[element].end,
                              &CD,
                              &length,
@@ -564,12 +564,12 @@ print_concordance_line(FILE *outfd,
 
   fputs(outstr, outfd);
   free(outstr);
-  
+
   if (pdr->AfterLine)
     fputs(pdr->AfterLine, outfd);
-  
+
   if (CD.alignedCorpora != NULL)
-    printAlignedStrings(cl->corpus, 
+    printAlignedStrings(cl->corpus,
                         &CD,
                         cl->range[element].start, cl->range[element].end,
                         apply_highlighting,
@@ -577,10 +577,13 @@ print_concordance_line(FILE *outfd,
 }
 
 
-
-void 
-ascii_print_corpus_header(CorpusList *cl, 
-                          FILE *stream)
+/**
+ * Prints a header for a "cat" command.
+ *
+ * Note that the "corpus" here refers to a subcorpus IE query result.
+ */
+void
+ascii_print_corpus_header(CorpusList *cl, FILE *stream)
 {
   time_t now;
 
@@ -589,7 +592,7 @@ ascii_print_corpus_header(CorpusList *cl,
 #endif
 
   int i;
-  
+
   time(&now);
   /*   pwd = getpwuid(geteuid()); */
   /* disabled because of incompatibilities between different Linux versions */
@@ -598,7 +601,7 @@ ascii_print_corpus_header(CorpusList *cl,
   for (i = 0; i < 75; i++)
     fputc('-', stream);
   fputc('\n', stream);
-  
+
   fprintf(stream,
           "#\n"
           "# User:    %s (%s)\n"
@@ -629,20 +632,20 @@ ascii_print_corpus_header(CorpusList *cl,
           (CD.right_type == CHAR_CONTEXT) ? "characters" :
           ((CD.right_type == WORD_CONTEXT) ? "words" :
            (CD.right_structure_name) ? CD.right_structure_name : "???"));
-  
+
   if (cl->query_corpus && cl->query_text) {
     fprintf(stream, "# Query: %s; %s\n", cl->query_corpus, cl->query_text);
   }
-  
-  
+
+
   fputc('#', stream);
   for (i = 0; i < 75; i++)
     fputc('-', stream);
   fputc('\n', stream);
 }
 
-void 
-ascii_print_output(CorpusList *cl, 
+void
+ascii_print_output(CorpusList *cl,
                    FILE *outfd,
                    int interactive,
                    ContextDescriptor *cd,
@@ -657,7 +660,7 @@ ascii_print_output(CorpusList *cl,
     last = cl->size - 1;
 
   for (i = first; (i <= last) && !cl_broken_pipe; i++) {
-    
+
     if (cl->sortidx)
       real_line = cl->sortidx[i];
     else
@@ -674,7 +677,7 @@ ascii_print_output(CorpusList *cl,
   }
 }
 
-void 
+void
 ascii_print_group(Group *group, int expand, FILE *fd)
 {
   int source_id, target_id, count;
@@ -694,7 +697,7 @@ ascii_print_group(Group *group, int expand, FILE *fd)
 
     source_id = group->count_cells[cell].s;
     source_s = Group_id2str(group, source_id, 0);
-   
+
     target_id = group->count_cells[cell].t;
     target_s = Group_id2str(group, target_id, 1);
     count     = group->count_cells[cell].freq;
@@ -708,19 +711,19 @@ ascii_print_group(Group *group, int expand, FILE *fd)
       /* separator bar between groups */
       if (cell == 0 || (group->is_grouped && nr_targets == 0))
         fprintf(fd, SEPARATOR);
-      
+
       fprintf(fd, "%-28s  %-28s\t%6d\n",
               (nr_targets == 0) ? source_s : " ", target_s, count);
     }
     else {
       if (source_id < 0) source_s = "";        /* don't print "(none)" or "(all)" in plain mode (just empty string) */
       if (target_id < 0) target_s = "";
-      if (has_source) 
+      if (has_source)
         fprintf(fd, "%s\t%s\t%d\n", source_s, target_s, count);
-      else 
+      else
         fprintf(fd, "%s\t%d\n", target_s, count);
     }
-    
+
     if (expand) {
       /* Ausgabe der entsprechenden Konkordanzzeilen??? */
     }

commit 32506d668e14f42786adba311a4631fb38e29019

CWB v3.4.16: Experimental feature allowing multiple target markers @0 .. @9. Only one of these markers is "active", as determined by the AnchorNumberTarget option, and corresponds to the @ marker. (work in progress)
Fixed handling of parsing errors in CQP queries, which was broken by r1075 (because synchronize()<cqp/parser.y> was inadvertently disabled).
   


git-svn-id: https://svn.code.sf.net/p/cwb/code/cwb/trunk@1165 b72dbb9a-5b02-45d8-a000-b69eecba7de2

cqp/options.c
@@ -110,10 +110,10 @@ CQPOption cqpoptions[] = {
   { "hf", "HistoryFile",          OptString,  &cqp_history_file,       NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "wh", "WriteHistory",         OptBoolean, &write_history_file,     NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "ms", "MatchingStrategy",     OptString,  &matching_strategy_name, "standard",   0,   NULL,   9,     OPTION_VISIBLE_IN_CQP },
-  { "sr", "StrictRegions",        OptBoolean, &strict_regions,         NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP},
-  { "p",  "Paging",               OptBoolean, &paging,                 NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP},
+  { "sr", "StrictRegions",        OptBoolean, &strict_regions,         NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
+  { "p",  "Paging",               OptBoolean, &paging,                 NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
 #ifndef __MINGW__
-  { "pg", "Pager",                OptString,  &pager,                  "less -FRX -+S",0, "CQP_PAGER",0, OPTION_VISIBLE_IN_CQP},
+  { "pg", "Pager",                OptString,  &pager,                  "less -FRX -+S",0, "CQP_PAGER",0, OPTION_VISIBLE_IN_CQP },
   { "h",  "Highlighting",         OptBoolean, &highlighting,           NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
 #else
   /* use more as default pager under Windows (because it exists whereas less may not :-P ) */
@@ -129,14 +129,15 @@ CQPOption cqpoptions[] = {
   { "rc", "RightContext",         OptContext, &CD,                     NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "ld", "LeftKWICDelim",        OptString,  &left_delimiter,         "<",          0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "rd", "RightKWICDelim",       OptString,  &right_delimiter,        ">",          0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
-  { "pm", "PrintMode",            OptString,  &printModeString,        "ascii",      0,   NULL,   6,     OPTION_VISIBLE_IN_CQP},
-  { "po", "PrintOptions",         OptString,  &printModeOptions,       NULL,         0,   NULL,   8,     OPTION_VISIBLE_IN_CQP},
-  { "ps", "PrintStructures",      OptString,  &printStructure,         NULL,         0,   NULL,   7,     OPTION_VISIBLE_IN_CQP},
-  { "sta","ShowTagAttributes",    OptBoolean, &show_tag_attributes,    NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP},
-  { "st", "ShowTargets",          OptBoolean, &show_targets,           NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP},
+  { "pm", "PrintMode",            OptString,  &printModeString,        "ascii",      0,   NULL,   6,     OPTION_VISIBLE_IN_CQP },
+  { "po", "PrintOptions",         OptString,  &printModeOptions,       NULL,         0,   NULL,   8,     OPTION_VISIBLE_IN_CQP },
+  { "ps", "PrintStructures",      OptString,  &printStructure,         NULL,         0,   NULL,   7,     OPTION_VISIBLE_IN_CQP },
+  { "sta","ShowTagAttributes",    OptBoolean, &show_tag_attributes,    NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
+  { "st", "ShowTargets",          OptBoolean, &show_targets,           NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "as", "AutoShow",             OptBoolean, &autoshow,               NULL,         1,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { NULL, "Timing",               OptBoolean, &timing,                 NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "o",  "Optimize",             OptBoolean, &query_optimize,         NULL,         0,   NULL,   3,     OPTION_VISIBLE_IN_CQP },
+  { "ant","AnchorNumberTarget",   OptInteger, &anchor_number_target,   NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "es", "ExternalSort",         OptBoolean, &UseExternalSorting,     NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "esc","ExternalSortCommand",  OptString,  &ExternalSortingCommand, NULL,         0,   NULL,   0,     OPTION_VISIBLE_IN_CQP },
   { "da", "DefaultNonbrackAttr",  OptString,  &def_unbr_attr,          DEFAULT_ATT_NAME,0,NULL,   0,     OPTION_VISIBLE_IN_CQP },
@@ -646,6 +647,20 @@ execute_side_effects(int opt)
 }
 
 int
+validate_integer_option_value(int opt, int value)
+{
+  char* optname = cqpoptions[opt].opt_name;
+  if (strcmp(optname, "AnchorNumberTarget") == 0) {
+    if (value < 0 || value > 9) {
+      cqpmessage(Warning, "set %s must be integer in range 0 .. 9", optname);
+      return 0;
+    }
+  }
+  return 1;
+}
+
+
+int
 validate_string_option_value(int opt, char *value)
 {
 #ifdef __NEVER__
@@ -695,13 +710,6 @@ validate_string_option_value(int opt, char *value)
 
   return 1;
 }
-
-int
-validate_integer_option_value(int opt, int value)
-{
-  return 1;
-}
-
 
 /**
  * Sets a string-valued option.

cqp/options.h
@@ -91,6 +91,7 @@ int hard_cut;                     /**< Query option: use hard cut value for all 
 int auto_subquery;                /**< Query option: use auto-subquery mode */
 char *def_unbr_attr;              /**< Query option: unbracketed attribute (attribute matched by "..." patterns) */
 int query_optimize;               /**< Query option: use query optimisation (untested and expensive optimisations) */
+int anchor_number_target;         /**< Query option: which marker @0 ... @9 will be mapped to the target anchor */
 
 /** matching strategy */
 enum _matching_strategy { traditional, shortest_match, standard_match, longest_match } matching_strategy;

cqp/parser.l
@@ -274,9 +274,6 @@ RE\(                    { return(RE_PAREN); }  /* for [lemma = RE($var)] */
                             yylval.strval[yyleng-1] = '\0';
                           return(LABEL);
                         }
-@:                      { /* allowing @: targets makes distinction between labels & target in macros unnecessary */
-                          return('@');
-                        }
 {nqrid}                 { 
                           yylval.strval = cl_strdup(yytext);
                           return(NQRID);

cqp/parser.y
@@ -89,7 +89,7 @@ synchronize(void)
 {
 /* function may be left uncompiled by defining this constant
  * in the make-configuration file. See the Makefile.         */
-#ifdef CQP_COMPILE_WITHOUT_SYNCHRONIZE
+#ifndef CQP_COMPILE_WITHOUT_SYNCHRONIZE
 
   int macro_status; /*  stores enable_macros status */
 
@@ -323,7 +323,7 @@ synchronize(void)
 %type <cl> StandardQuery TABQuery
 
 %type <strval> EmbeddedModifier
-%type <ival> OptTargetSign
+%type <ival> OptTargetSign TargetNumber
 %type <apl> FunctionArgList SingleArg
 %type <varval> VarValue
 %type <rngsetop> SetOp
@@ -1107,10 +1107,34 @@ NamedWfPattern: OptTargetSign
                 WordformPattern         { $$ = do_NamedWfPattern($1, $2, $3); }
                 ;
 
-
-OptTargetSign:    '@'                   { $$ = 1; }
+/* New experimental feature in v3.4.16: numbered target markers @0 .. @9
+ *  - one of these markers is mapped to the target anchor (determined by AnchorNumberTarget option)
+ *  - all other numbered target markers are silently ignored
+ *  - @ by itself always stands for the target anchor (so backward compatibility is guaranteed)
+ *  - @:, @0: ... @9: allowed so CQP macros do not have to distinguish between labels and target anchors 
+ * See "Undocumented CQP" section of the CQP Query Tutorial for further explanations. */
+OptTargetSign:    '@' OptColon          { $$ = 1; }
+	            | '@' TargetNumber OptColon {
+	            						  if ($2 == anchor_number_target)
+	            						    $$ = 1;
+	            						  else
+	            						  	$$ = 0; 
+	            						}
                 | /* epsilon */         { $$ = 0; }
                 ;
+
+OptColon:         ':'
+                | /* epsilon */        
+                ;
+
+TargetNumber:   INTEGER					{ if ($1 < 0 || $1 > 9) {
+											yyerror("expected number between 0 and 9");
+											YYERROR;
+										  }
+										  $$ = $1;
+										}
+				;
+
 
 OptRefId:         LABEL                 { $$ = $1; }
                 | /* epsilon */         { $$ = NULL; }

definitions.mk
@@ -23,7 +23,7 @@
 #
 # CWB version
 #
-VERSION = 3.4.15
+VERSION = 3.4.16
 # 3.4.x = beta versions leading up to new stable 3.5.0
 
 #